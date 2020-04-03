44bfa720e0f5e3e529a602a47079cc0ff5c1ffd1
ABdul <a.abdulrahman41@gmail.com>
2020-04-03 10:23:15 +0000

    BackupDat

---
+++ jobs/MavenGitProject/cobertura/artifactory/test/Multi1.java
@@ -0,0 +1,50 @@
+<tr class="noCover">
+<td class="line"><a name='1'/>1</td>
+<td class="hits"/>
+<td class="code">package&nbsp;artifactory.test;</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='2'/>2</td>
+<td class="hits"/>
+<td class="code"></td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='3'/>3</td>
+<td class="hits"/>
+<td class="code">/**</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='4'/>4</td>
+<td class="hits"/>
+<td class="code">&nbsp;This&nbsp;is&nbsp;check</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='5'/>5</td>
+<td class="hits"/>
+<td class="code">&nbsp;*/</td>
+</tr>
+<tr class="coverNone">
+<td class="line"><a name='6'/>6</td>
+<td class="hits">0</td>
+<td class="code">public&nbsp;class&nbsp;Multi1&nbsp;{</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='7'/>7</td>
+<td class="hits"/>
+<td class="code">&nbsp;&nbsp;&nbsp;&nbsp;public&nbsp;static&nbsp;void&nbsp;main(String[]&nbsp;args)&nbsp;{</td>
+</tr>
+<tr class="coverNone">
+<td class="line"><a name='8'/>8</td>
+<td class="hits">0</td>
+<td class="code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;System.out.println("Hello&nbsp;World!");</td>
+</tr>
+<tr class="coverNone">
+<td class="line"><a name='9'/>9</td>
+<td class="hits">0</td>
+<td class="code">&nbsp;&nbsp;&nbsp;&nbsp;}</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='10'/>10</td>
+<td class="hits"/>
+<td class="code">}</td>
+</tr>
